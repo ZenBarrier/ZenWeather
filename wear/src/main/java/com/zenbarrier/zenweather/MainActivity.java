8b03d8af2a887112dd19b300cb5e2feb6d7a1acd
ZenBarrier <anthony.j.barrera@gmail.com>
2017-05-05 16:59:18 -0400

    Added action drawer

--- wear/src/main/java/com/zenbarrier/zenweather/MainActivity.java
+++ wear/src/main/java/com/zenbarrier/zenweather/MainActivity.java
@@ -3,7 +3,12 @@ package com.zenbarrier.zenweather;
 import android.app.Activity;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.wearable.view.drawer.WearableActionDrawer;
+import android.support.wearable.view.drawer.WearableDrawerLayout;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.zenbarrier.mylibrary.GetLocationTask;
@@ -14,20 +19,29 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends Activity implements WeatherTask.WeatherTaskInterface,
-        GetLocationTask.LocationTaskInterface {
+        GetLocationTask.LocationTaskInterface, WearableActionDrawer.OnMenuItemClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private TextView mTextView;
+    private WearableDrawerLayout mWearableDrawerLayout;
+    private WearableActionDrawer mWearableActionDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.textView_main);
+        mWearableDrawerLayout = (WearableDrawerLayout) findViewById(R.id.drawer_layout);
+        mWearableDrawerLayout.peekDrawer(Gravity.BOTTOM);
+
+        mTextView = (TextView) findViewById(R.id.textView_main_temp);
 
         GetLocationTask locationTask = new GetLocationTask(this);
         locationTask.execute();
+
+        mWearableActionDrawer = (WearableActionDrawer) findViewById(R.id.bottom_action_drawer);
+        mWearableActionDrawer.setOnMenuItemClickListener(this);
+        mWearableActionDrawer.setTitle("title");
 
     }
 
@@ -50,5 +64,18 @@ public class MainActivity extends Activity implements WeatherTask.WeatherTaskInt
         Log.d(TAG, location.toString());
         WeatherTask weatherTask = new WeatherTask(this);
         weatherTask.execute(location.getLatitude(), location.getLongitude());
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.menu_location:
+                break;
+            case R.id.menu_temperature_unit:
+                break;
+            default:
+                return false;
+        }
+        return true;
     }
 }
