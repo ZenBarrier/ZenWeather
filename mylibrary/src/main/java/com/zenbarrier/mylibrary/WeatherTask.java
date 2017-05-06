682377f13596073c89ee14a48a4ac9bb010053d8
ZenBarrier <anthony.j.barrera@gmail.com>
2017-05-05 20:20:14 -0400

    Limit API weather call to once an hour and store result in shred pref. Otherwise return from shared prefs.

--- mylibrary/src/main/java/com/zenbarrier/mylibrary/WeatherTask.java
+++ mylibrary/src/main/java/com/zenbarrier/mylibrary/WeatherTask.java
@@ -1,7 +1,9 @@
 package com.zenbarrier.mylibrary;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,20 +13,34 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Locale;
 
-/**
- * Created by Anthony on 5/3/2017.
- * This file is the fragment that holds all the preferences
- */
-
 public class WeatherTask extends AsyncTask<Double, Void, String> {
     private Context mContext;
+    private SharedPreferences sharedPreferences;
+    private boolean hasUpdated = false;
+
+    private static final String KEY_PREF_WEATHER_TIME_STAMP = "KEY_PREF_WEATHER_TIME_STAMP";
+    private static final String KEY_PREF_WEATHER_JSON = "KEY_PREF_WEATHER_JSON";
+    private static final long ONE_HOUR_MS = 3600 * 1000;
 
     public interface WeatherTaskInterface {
         void onWeatherRetrieved(String result);
     }
 
     @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mContext);
+    }
+
+    @Override
     protected String doInBackground(Double... params) {
+
+        long timeStamp = sharedPreferences.getLong(KEY_PREF_WEATHER_TIME_STAMP, System.currentTimeMillis());
+        long currentTimeStamp = System.currentTimeMillis();
+        if((currentTimeStamp - timeStamp) >= ONE_HOUR_MS){
+            return sharedPreferences.getString(KEY_PREF_WEATHER_JSON, "");
+        }
+
         double lat = params[0];
         double lng = params[1];
         String appId = mContext.getString(R.string.weather_api);
@@ -49,18 +65,23 @@ public class WeatherTask extends AsyncTask<Double, Void, String> {
                 stringBuilder.append(line);
             }
 
+            hasUpdated = true;
             return stringBuilder.toString();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return "";
+        return sharedPreferences.getString(KEY_PREF_WEATHER_JSON, "");
     }
 
     @Override
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
+        if(hasUpdated && result.length() > 0) {
+            sharedPreferences.edit().putLong(KEY_PREF_WEATHER_TIME_STAMP, System.currentTimeMillis())
+                    .putString(KEY_PREF_WEATHER_JSON, result).apply();
+        }
         if(mContext instanceof WeatherTaskInterface){
             ((WeatherTaskInterface) mContext).onWeatherRetrieved(result);
         }
